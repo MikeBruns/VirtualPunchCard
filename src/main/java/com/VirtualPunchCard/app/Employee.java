362f9500905ccd01fd6600f4ad4db0e1a84e5c05
Michael Bruns <mbruns@bgsu.edu>
2017-04-01 21:14:51 -0400

    created base employee class

--- src/main/java/com/VirtualPunchCard/app/Employee.java
+++ src/main/java/com/VirtualPunchCard/app/Employee.java
@@ -1,8 +1,65 @@
 package com.VirtualPunchCard.app;
 
+import java.sql.Date;
+
 /**
  * Class describing an employee
  */
 public class Employee {
-    //TODO Write Class
+    private int     employeeID;
+    private Date    dob;
+    private Date    hireDate;
+    private String  firstName;
+    private String  lastName;
+
+    public Employee(int employeeID, Date dob, Date hireDate,
+                    String firstName, String lastName) {
+        this.employeeID = employeeID;
+        this.dob = dob;
+        this.hireDate = hireDate;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
